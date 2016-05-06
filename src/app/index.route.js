de22cc1a6e26ab04f6d18f695e4fa2c07ec9b509
shiva <shiva@gmail.com>
2016-05-06 17:51:19 +0530

    First commit

---
+++ src/app/index.route.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('pip')
+    .config(routerConfig);
+
+  /** @ngInject */
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController'
+      })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/views/login.html',
+            controller: 'LoginController'
+        });
+
+    $urlRouterProvider.otherwise('/');
+  }
+
+})();
